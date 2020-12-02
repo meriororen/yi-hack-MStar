0aa4f00ab3c77e9f081bdeecacf66f93bf79fac9
Isa Ansharullah <meriororen@gmail.com>
2020-12-02 11:25:48 +0700

    Add jump to point argument on ptz.sh

--- src/www/httpd/cgi-bin/ptz.sh
+++ src/www/httpd/cgi-bin/ptz.sh
@@ -2,8 +2,11 @@
 
 DIR="none"
 TIME="0.3"
+POINT="none"
+XPOINT="-1"
+YPOINT="-1"
 
-for I in 1 2
+for I in 1 2 3
 do
     CONF="$(echo $QUERY_STRING | cut -d'&' -f$I | cut -d'=' -f1)"
     VAL="$(echo $QUERY_STRING | cut -d'&' -f$I | cut -d'=' -f2)"
@@ -12,6 +15,10 @@ do
         DIR="-m $VAL"
     elif [ "$CONF" == "time" ] ; then
         TIME="$VAL"
+    elif [ "$CONF" == "point" ] ; then
+	POINT="$VAL"
+	XPOINT="$(echo $VAL | cut -d',' -f1)"
+	YPOINT="$(echo $VAL | cut -d',' -f2)"
     fi
 done
 
@@ -21,7 +28,12 @@ if [ "$DIR" != "none" ] ; then
     ipc_cmd -m stop
 fi
 
+if [ "$POINT" != "none" ] ; then
+    ipc_cmd -j $VAL || ipc_cmd -j $XPOINT,$YPOINT
+fi
+
 printf "Content-type: application/json\r\n\r\n"
 
 printf "{\n"
+printf "direction=$DIR,time=$TIME,pointX=$XPOINT,pointY=$YPOINT\n"
 printf "}"
